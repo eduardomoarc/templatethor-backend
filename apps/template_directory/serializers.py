f768d168fa1c40c57ff949c36c1c49ababd8d666
Eduardo Moron <eduardo.moron.arce@gmail.com>
2025-01-31 23:47:36 -0400

    feat: models relations received and returned by guid

--- apps/template_directory/serializers.py
+++ apps/template_directory/serializers.py
@@ -1,13 +1,45 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from apps.template_directory.models import TemplateDirectory
+from apps.template_group.models import TemplateGroup
 
 
 class TemplateDirectorySerializer(serializers.ModelSerializer):
     uuid = serializers.CharField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
+    template_group_uuid = serializers.CharField(write_only=False, source="template_group.uuid")
+    parent_directory_uuid = serializers.CharField(source="parent_directory.uuid", required=False, allow_null=True)
 
     class Meta:
         model = TemplateDirectory
-        fields = ['uuid', 'name', 'parent_directory', 'template_group', 'created_at', 'updated_at']
+        fields = ['uuid', 'name', 'parent_directory_uuid', 'template_group_uuid', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        validated_data['template_group'] = self._get_template_group_from_data(validated_data)
+        validated_data['parent_directory'] = self._get_parent_directory_from_data(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'template_group' in validated_data:
+            instance.template_group = self._get_template_group_from_data(validated_data)
+
+        if 'parent_directory' in validated_data:
+            instance.parent_directory = self._get_parent_directory_from_data(validated_data)
+        return super().update(instance, validated_data)
+
+    @staticmethod
+    def _get_template_group_from_data(validated_data):
+        template_group_data = validated_data.pop('template_group', {})
+        template_group_uuid = template_group_data.get('uuid')
+        if not template_group_uuid:
+            raise serializers.ValidationError({"template_group_uuid": "This field is required."})
+        return get_object_or_404(TemplateGroup, uuid=template_group_uuid)
+
+    @staticmethod
+    def _get_parent_directory_from_data(validated_data):
+        parent_directory_data = validated_data.pop('parent_directory', {})
+        parent_directory_uuid = parent_directory_data.get('uuid')
+        if parent_directory_uuid:
+            return get_object_or_404(TemplateDirectory, uuid=parent_directory_uuid)
--- apps/template_file/serializers.py
+++ apps/template_file/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
+from apps.template_directory.models import TemplateDirectory
 from apps.template_file.models import TemplateFile
 
 
@@ -7,7 +9,27 @@ class TemplateFileSerializer(serializers.ModelSerializer):
     uuid = serializers.CharField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
+    template_directory_uuid = serializers.CharField(source="template_directory.uuid")
+    content = serializers.CharField(allow_blank=True)
 
     class Meta:
         model = TemplateFile
-        fields = ['uuid', 'name', 'type', 'content', 'template_directory', 'created_at', 'updated_at', ]
+        fields = ['uuid', 'name', 'type', 'content', 'template_directory_uuid', 'created_at', 'updated_at', ]
+
+    def create(self, validated_data):
+        validated_data['template_directory'] = self._get_template_directory_from_data(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'template_directory' in validated_data:
+            instance.template_directory = self._get_template_directory_from_data(validated_data)
+        return super().update(instance, validated_data)
+
+    @staticmethod
+    def _get_template_directory_from_data(validated_data):
+        template_directory_data = validated_data.pop('template_directory', {})
+        template_directory_uuid = template_directory_data.get('uuid')
+        if not template_directory_uuid:
+            raise serializers.ValidationError({"template_directory_uuid": "This field is required."})
+        return get_object_or_404(TemplateDirectory, uuid=template_directory_uuid)
+
